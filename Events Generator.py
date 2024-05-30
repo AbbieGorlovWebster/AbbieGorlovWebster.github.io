ee4283c9b0d7f5785d1e11f1f9105385d083ded7
Abbie Gorlov-Webster <AbbiegorlovWebster@gmail.com>
2024-05-30 15:55:38 +0100

    Feature Complete

--- Events Generator.py
+++ Events Generator.py
@@ -6,30 +6,39 @@ with open('E:\Sheppey Is Ours Website\AbbieGorlovWebster.github.io\Events.csv') 
     
     next(csv_reader)
 
-    for row in csv_reader:
-        print("\t<div class=\"hl Width80\"></div>\n")
+    with open('E:\Sheppey Is Ours Website\AbbieGorlovWebster.github.io\Events Output.txt', 'w') as f:
         
-        print("\t<div class=\"Event Width80\">")
-        print("\t\t<div class=\"FlexRow\">")
-        print("\t\t\t<div class=\"TextContainer\" id=\"Date\">")
-        print("\t\t\t\t<p class=\"TextCenter BlackText Weight700 Size24 TextLeft\">", row[1], "</p>", sep="")
-        print("\t\t\t</div>\n")
+        for row in csv_reader:
+            print("\t<div class=\"hl Width80\"></div>\n", file=f)
+            
+            print("\t<div class=\"Event Width80\">", file=f)
+            print("\t\t<div class=\"FlexRow\">", file=f)
+            print("\t\t\t<div class=\"TextContainer\" id=\"Date\">", file=f)
+            print("\t\t\t\t<p class=\"TextCenter BlackText Weight700 Size24 TextLeft\">", row[1], "</p>", sep="", file=f)
+            print("\t\t\t</div>\n", file=f)
+            
+            print("\t\t\t<div class=\"TextContainer\" id=\"Details\">", file=f)
+            print("\t\t\t\t<h2 class=\"TextCenter BlackText Weight700 Size24 TextLeft\">", row[0], "</h2>", sep="", file=f)
+            
+            if(row[6]!=""):
+                print("\t\t\t\t<input type=\"checkbox\" id=\"EventToggle",line_count,"\" /> <label for=\"EventToggle",line_count,"\" class=\"EventToggleLabel Weight700\">Event Details</label>", sep="", file=f)
+                print("\t\t\t\t<div class=\"ExpandableDetails\">", file=f)
+                print("\t\t\t\t\t<p class=\"TextCenter BlackText Weight400 Size18 TextLeft\">",row[6],"</p>",sep="", file=f)
+                print("\t\t\t\t</div>", file=f)
+            print("\t\t\t</div>\n", file=f)
+            
+            print("\t\t\t<div class=\"TextContainer\" id=\"Time\">", file=f)
+            if(row[3] != ""):
+                print("\t\t\t\t<p class=\"TextCenter BlackText Weight700 Size24 TextRight\">",row[2]," - ",row[3],"</p>",sep="", file=f)
+            else:
+                print("\t\t\t\t<p class=\"TextCenter BlackText Weight700 Size24 TextRight\">",row[2],"</p>",sep="", file=f)
+                
+            print("\t\t\t\t<p class=\"TextCenter BlackText Weight400 Size18 TextRight\">",row[4],"<br>",row[5],"</p>",sep="", file=f)
+            print("\t\t\t</div>", file=f)
+            print("\t\t</div>", file=f)
+            print("\t</div>\n", file=f)
+            
+            line_count += 1
         
-        print("\t\t\t<div class=\"TextContainer\" id=\"Details\">")
-        print("\t\t\t\t<h2 class=\"TextCenter BlackText Weight700 Size24 TextLeft\">", row[0], "</h2>", sep="")
-        print("\t\t\t\t<input type=\"checkbox\" id=\"EventToggle",line_count,"\" /> <label for=\"EventToggle",line_count,"\" class=\"EventToggleLabel Weight700\">Event Details</label>", sep="")
-        print("\t\t\t\t<div class=\"ExpandableDetails\">")
-        print("\t\t\t\t\t<p class=\"TextCenter BlackText Weight400 Size18 TextLeft\">",row[5],"</p>",sep="")
-        print("\t\t\t\t</div>")
-        print("\t\t\t</div>\n")
-        
-        print("\t\t\t<div class=\"TextContainer\" id=\"Time\">")
-        print("\t\t\t\t<p class=\"TextCenter BlackText Weight700 Size24 TextRight\">",row[2],"</p>",sep="")
-        print("\t\t\t\t<p class=\"TextCenter BlackText Weight400 Size18 TextRight\">",row[3],"<br>",row[4],"</p>",sep="")
-        print("\t\t\t</div>")
-        print("\t\t</div>")
-        print("\t</div>\n")
-        
-        line_count += 1
+        print("\t<div class=\"hl Width80\"></div>", file=f)
 
-print("\t<div class=\"hl Width80\"></div>")
